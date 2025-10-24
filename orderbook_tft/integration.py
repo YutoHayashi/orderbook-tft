ca1192458c72b8d2e9bf814e2f00046868095c17
YutoHayashi <yutohayashi@hotmail.com>
2025-10-24 17:45:35 +0900

    NormalizerがGPUにいるとエラーになる問題の修正

--- orderbook_tft/integration.py
+++ orderbook_tft/integration.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     import pandas as pd
     from pytorch_forecasting import TimeSeriesDataSet
     
-    from data_processor import prepare_dataframe
+    from .data_processor import prepare_dataframe
     
     # Example usage
     batch_size = 1
@@ -31,24 +31,33 @@ if __name__ == "__main__":
     hparams = tft.hparams
     
     dataset_parameters = hparams.get('dataset_parameters')
+    
+    target = dataset_parameters.get('target')
     max_encoder_length = dataset_parameters.get('max_encoder_length')
     max_prediction_length = dataset_parameters.get('max_prediction_length')
-    time_varying_unknown_reals = dataset_parameters.get('time_varying_unknown_reals')
+    target_normalizer = dataset_parameters.get('target_normalizer')
     
     df = pd.read_csv('csv/board_snapshots.csv')
     df = prepare_dataframe(df)
-    df = df.filter(items=time_varying_unknown_reals)
     df['time_idx'] = range(len(df))
     df['series_id'] = 0
     df = df[lambda x: x.time_idx > (x.time_idx.max() - max_encoder_length)]
     
+    unknown_cols = [col for col in df.columns if col not in ['time_idx', 'series_id']]
+    
     df = pd.concat([df, pd.DataFrame({
         'time_idx': range(df['time_idx'].max() + 1, df['time_idx'].max() + 1 + max_prediction_length),
         'series_id': 0,
-        **{col: np.float32(0) for col in time_varying_unknown_reals},
+        **{col: np.float32(0) for col in unknown_cols},
     })], ignore_index=True)
     
     print(f"Dataframe shape: {df.shape}")
+    
+    for normalizer in target_normalizer.normalizers:
+        for attr_name, value in vars(normalizer).items():
+            if isinstance(value, torch.Tensor):
+                setattr(normalizer, attr_name, value.cpu())
+    dataset_parameters['target_normalizer'] = target_normalizer
     
     dataset = TimeSeriesDataSet.from_parameters(dataset_parameters, df, predict=True)
     print(f"Number of samples in the dataset: {len(dataset)}")
